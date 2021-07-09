2110372e29881fd8e0f6ce0ad0660106c7caadd8
Gavin Whitlock <gavinwhitlock@atx-gwhitlock.lan>
2021-07-09 15:35:28 -0500

    styled new post page

--- app/reflexes/elements_reflex.rb
+++ app/reflexes/elements_reflex.rb
@@ -7,6 +7,8 @@ class ElementsReflex < ApplicationReflex
             element_record = Element.find(element['id'])
             element_record.update(position: element['position'])
         end
+        #NOTE: This is a hack! Might break with future versions of stimulus reflex. This instance varible prevent the rest of the rendering (after the above block executes) from happening (and keeps state of each element). 
+        @halted = true
     end
 
 end
